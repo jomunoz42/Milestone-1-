f320b45c567219bacbd1f7e057f1629d66e96f24
jomunoz42 <jpmunoz@sapo.pt>
2025-05-20 21:57:24 +0100

    Add files via upload

---
+++ get_next_line/get_next_line_bonus.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jomunoz <jomunoz@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/14 21:21:41 by jomunoz           #+#    #+#             */
+/*   Updated: 2025/05/17 20:19:29 by jomunoz          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <fcntl.h>
+# include <limits.h>
+# include <stdarg.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 5
+# endif
+
+char	*get_next_line(int fd);
+size_t	get_size(const char *s);
+int		if_new_line_exists(char *s);
+void	get_the_rest(char *buffer);
+char	*copy_to_line(char *line, char *buffer);
+
+#endif
